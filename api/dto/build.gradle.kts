d3f05e213321193a38d58eee69fff420c59c1b48
Artur Tuzim <artur@tuzim.xyz>
2026-04-03 17:03:00 +0200

    chore(gradle): Adjust dependencies in `:api:dto` for feature annotations and implementations

--- api/dto/build.gradle.kts
+++ api/dto/build.gradle.kts
@@ -11,12 +11,10 @@ kotlin {
         compilerOptions.jvmDefault = JvmDefaultMode.NO_COMPATIBILITY
     }
     sourceSets.commonMain {
-        kotlin.srcDir("build/generated/ksp/metadata/commonMain/kotlin")
         dependencies {
-            implementation(projects.api.feature.annotations)
-
+            api(projects.api.feature.annotations)
+            api(projects.api.feature.implementations)
             api(libs.kotlinx.serialization.json)
-
             api(projects.api.feature.common)
         }
     }
